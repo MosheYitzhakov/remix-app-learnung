87c2562c9890f8e6b0b4233812a5cab236cff6bb
Shimon Ris <shimonr@ravtech.co.il>
2025-02-05 10:32:03 +0200

    lesson 16

--- work-journal/app/routes/login.tsx
+++ work-journal/app/routes/login.tsx
@@ -1,9 +1,10 @@
 import {
+  json,
   redirect,
   type ActionArgs,
   type LoaderArgs,
 } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { commitSession, getSession } from "~/session";
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
@@ -12,19 +13,20 @@ export async function action({ request }: ActionArgs) {
     let session = await getSession();
     session.set("isAdmin", true);
     return redirect("/", {
-        headers: { "Set-Cookie": await commitSession(session) },
-      });
+      headers: { "Set-Cookie": await commitSession(session) },
+    });
   } else {
-    return null;
+    return json({ error: "Invalid login" }, 401);
   }
 }
 export async function loader({ request }: LoaderArgs) {
-    let session = await getSession(request.headers.get("cookie"));
-  
-    return session.data;
-  }
+  let session = await getSession(request.headers.get("cookie"));
+
+  return session.data;
+}
 export default function LoginPage() {
-    let data = useLoaderData<typeof loader>();
+  let data = useLoaderData<typeof loader>();
+  const actionData = useActionData();
   return (
     <div className="mt-8">
       {data?.isAdmin ? (
@@ -36,17 +38,22 @@ export default function LoginPage() {
             type="email"
             name="email"
             placeholder="...email"
+            required
           />
           <input
             className="text-gray-900"
             type="password"
             name="password"
             placeholder="...password"
+            required
           />
           <button className="bg-blue-500 px-3 py-2 font-medium text-white">
             Log-in
           </button>
         </Form>
+      )}
+      {actionData?.error && (
+        <p className="mt-4 font-medium text-red-500">{actionData.error}</p>
       )}
     </div>
   );
